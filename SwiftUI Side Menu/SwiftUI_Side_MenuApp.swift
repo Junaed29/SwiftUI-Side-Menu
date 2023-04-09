e7edc3fa50274e1b13033ae4842e586e39d957fe
Junaed <junaed1995@gmail.com>
2023-04-09 17:02:46 +0600

    Initial Commit

---
+++ SwiftUI Side Menu/SwiftUI_Side_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Side_MenuApp.swift
+//  SwiftUI Side Menu
+//
+//  Created by Junaed Muhammad Chowdhury on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Side_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
